285cb400c9733202f52d5f1791c8ca3f4f470275
Allespro <39953296+Allespro@users.noreply.github.com>
2019-02-18 15:14:46 +0000

    Update KinematicPlayer.gd

--- KinematicPlayer.gd
+++ KinematicPlayer.gd
@@ -10,7 +10,7 @@ func _ready():
 	print (global_position)
 
 func _physics_process(delta):
-	
+	#************left right buttons************
 	if Input.is_action_pressed('ui_left'):
 		vel.x = -SPEED
 	elif Input.is_action_pressed('ui_right'):
@@ -19,19 +19,21 @@ func _physics_process(delta):
 		vel.x = 0
 	
 	vel.y += GRAV
+	#************jump when on plank************
 	if is_on_floor(): #&& Input.is_action_just_pressed('ui_up'):
 		vel.y = -JUMP ;
 		
 	vel = move_and_slide(vel, Vector2(0, -1))
 	
 	
-	
+	#************teleport to another side of screen************
 	if position.x < -360:
 		position.x = 360
 	
 	if position.x > 385:
 		position.x = -360
 	
+	#************if player falls from screen************
 	if position.y > 480:
 		#print ('fail')
 		position.x= 0
