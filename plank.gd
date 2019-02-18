1bf13a1334e74615d29a7a78827a3302020c2302
Allespro <39953296+Allespro@users.noreply.github.com>
2019-02-18 15:19:13 +0000

    Update plank.gd

--- plank.gd
+++ plank.gd
@@ -5,14 +5,16 @@ var SPEED = 3.5
 #var SPEED_LEVEL = 80
 
 func _physics_process(delta):
+	#************Falling Down ((((( xxx&lil(((((************
 	position.y += SPEED
 	#if $"../../".score > SPEED_LEVEL:
 	#	SPEED += 0.05
 	#	SPEED_LEVEL += 70
-	
+	#************del if player don't see plank************
 	if position.y > 1281:
 		#print('del')
 		queue_free()
+	#************blink************
 	if $"../../".score > 32:
 		$AnimatedSprite.play()
 	
